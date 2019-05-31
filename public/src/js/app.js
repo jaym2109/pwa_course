e636c406383a925c87f2658ebec2c29be21f8a46
jaym2109 <jason@ontariopanelization.com>
2019-05-31 14:42:23 -0400

    service workers

---
+++ public/src/js/app.js
@@ -0,0 +1,14 @@
+let deferredPrompt;
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function() {
+    console.log("Service Worker Registered!");
+  });
+}
+
+window.addEventListener("beforeinstallprompt", function(event) {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
